0293113a2c6dfd69f50592dc16806ba4c43df2d3
dmardari <mdcoolcomrat@gmail.com>
2025-01-20 15:22:42 +0200

    Initial commit with Prisma support (can't be launched so far)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+use crate::prisma::PrismaClient;
 use axum::Router;
-
+use prisma_client_rust::NewClientError;
+#[allow(warnings)]
+mod prisma;
 mod routes;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
+
+    let prisma_client: Result<PrismaClient, NewClientError> =
+        PrismaClient::_builder().build().await;
 
     let app = Router::new().nest("/tasks", routes::create_route());
 
